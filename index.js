f1ba6a179eb1e2b6e899576702f40ba8ede87315
张业鹏 <hellolittlewhite@gmail.com>
2020-03-20 00:11:05 +0800

    finish demo

--- index.js
+++ index.js
@@ -5,20 +5,88 @@
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
+// const uuid = require('uuid');
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
-            res.writeHead(200, { 'Content-Type': 'text/html', 'maomao': 'zhangyepeng' })
-            res.end(content)
-        })
+    // if (req.url === '/') {
+    //     fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+    //         res.writeHead(200, { 'Content-Type': 'text/html', 'maomao': 'zhangyepeng' })
+    //         res.end(content)
+    //     })
+    // }
+    // if (req.url === '/about') {
+    //     fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
+    //         res.writeHead(200, { 'Content-Type': 'text/html', 'maomao': 'zhangyepeng' })
+    //         res.end(content)
+    //     })
+    // }
+
+    // if(req.url === '/api/users'){
+    //     const users = [
+    //         {name: 'Bob Smith', age: 40},
+    //         {name: 'John Doe', age: 28}
+    //     ];
+    //     res.writeHead(200, {'Content-Type': 'application/json'})
+    //     res.end(JSON.stringify(users)); 
+    // }
+
+    // Build file path
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url)
+    console.log(filePath)
+
+    // Extension of file
+    let extname = path.extname(filePath);
+
+    //Initial content type
+    let contentType = 'text/html';
+    
+    // Check ext and set content type
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+
+        case '.css':
+            contentType = 'text/css';
+            break;
+
+        case '.json':
+            contentType = 'application/json';
+            break;
+
+        case '.png':
+            contentType = 'image/png';
+            break;
+
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
     }
-    if (req.url === '/about') {
-        fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
-            res.writeHead(200, { 'Content-Type': 'text/html', 'maomao': 'zhangyepeng' })
-            res.end(content)
-        })
-    }
+    
+    // Read File
+    fs.readFile(filePath, (err, content) => {
+        if(err){
+            if(err.code == 'ENOENT'){
+                // Page not found
+                fs.readFile(path.join(__dirname, 'public', '404.html'),(err, content) => {
+                    res.writeHead(200, {
+                        'Content-Type':  'text/html'
+                    })
+                    res.end(content, 'utf8');
+                })
+            }else{
+                // Some server error
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+        }else{
+            // Success
+            res.writeHead(200, {
+                'Content-Type': contentType
+            })
+            res.end(content, 'utf8');
+        }
+    })
 })
 
 const PORT = process.env.PORT || 5000;
